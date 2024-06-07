5bfcbec3b0bc25e5781f4966f5538f8fc89b5b4d
Ashutosh <157938926+ShindeAsh@users.noreply.github.com>
2024-06-07 21:30:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const qrImage = document.getElementById('qr');
-const downloadLink = document.getElementById("dl");
-    function SendData() {
-        const name = document.getElementById("name").value;
-        const mobile = document.getElementById("mb").value;
-        const quantity = document.getElementById("quantity").value;
-        const data = `Name: ${name}, Mobile: ${mobile}, Quantity: ${quantity}`;
+updateQRCode("Write Something First")
+function SendData() {
+    const qrImage = document.getElementById('qr');
+    qrImage.src = "";
+    const name = document.getElementById("name").value;
+    const animal = document.getElementById("other").value;
+    var upName = String(name);
+    var newName = upName.toLowerCase();
+    if (newName.includes("ash")) {
+        const data = `The Person Who Typed this Name Is DONKEY`;
+        updateQRCode(data);
+    } else {
+        if (newName.includes("ash")){
+            const data = "im not allowing that"
+            updateQRCode(data);
+        }
+        const data = `${name} is a ${animal}`;
         updateQRCode(data);
     }
+}
 
-    function updateQRCode(newData) {
-        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?data=${newData}&size=200x200`;
-    }
+function updateQRCode(newData) {
+    const qrImage = document.getElementById('qr');
+    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(newData)}&size=200x200`;
+}
