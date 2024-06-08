50ec1119b441edb6833ae02f54a863268712b0ff
Ashutosh <157938926+ShindeAsh@users.noreply.github.com>
2024-06-08 11:53:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 const scanTxt = document.getElementById("scanTxt");
 updateQRCode("Write Something First")
 function SendData() {
-    const qrImage = document.getElementById('qr');
-    qrImage.src = "";
     const name = document.getElementById("name").value;
-    const animal = document.getElementById("other").value;
-    var upName = String(name);
-    var newName = upName.toLowerCase();
-    if (newName.includes("ash")) {
-        const data = `The Person Who Typed this Name Is DONKEY`;
-        updateQRCode(data);
-    } else {
-        if (newName.includes("ash")){
-            const data = "im not allowing that"
-            updateQRCode(data);
-        }
-        const data = `${name} is a ${animal}`;
-        updateQRCode(data);
-    }
+    const doing = document.getElementById("other").value;
+    var str = `${name} is doing ${doing}`;
+    updateQRCode(str);
     scanTxt.textContent = "Scan This";
 }
 
